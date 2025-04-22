39a16a9c25cdfe8ab22617eaa542eb68442bf5a7
”Anna <abarkova123@mail.ru>
2025-04-22 16:06:55 +0300

    Created getWeather override and update main func

--- WeatherService/JsonService.cpp
+++ WeatherService/JsonService.cpp
@@ -2,21 +2,21 @@
 #include <fstream>
 #include "json.hpp"
 using nlohmann::json;
-Weather JsonService::getWeather(std::string s)
-{
+
+Weather JsonService::getWeather(std::string s) {
     std::ifstream fin(s);
     if (!fin)
-        throw exception("error");
+        throw std::runtime_error("File error");
 
-    json j;
-    j = json::parse(fin);
-    std::string city = j["name"];          
-    double lon = j["coord"]["lon"];// 49.6601         
-    double lat = j["coord"]["lat"];    // 58.5966         
-    double temperature = j["main"]["temp"];   // 5.69   
-    std::string weather = j["weather"];  // дождь
-    double windSpeed = j["wind"]["speed"]; // 4.27
-    int clouds = j["clouds"]["all"];  // 100
+    json j = json::parse(fin);
+
+    std::string city = j["name"];
+    double lon = j["coord"]["lon"];
+    double lat = j["coord"]["lat"];         // 58.5966
+    double temperature = j["main"]["temp"]; // 5.69
+    std::string weather = j["weather"];     // дождь
+    double windSpeed = j["wind"]["speed"];   // 4.27
+    int clouds = j["clouds"]["all"];        // 100
 
     return Weather(city, lon, lat, temperature, weather, windSpeed, clouds);
 }
--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
+#include "JsonService.h"
 
 int main()
 {
-    
+    setlocale(LC_ALL, "rus");
+    JsonService js;
+    Weather w = js.getWeather("weather.txt");
+    w.print();
+
 }
