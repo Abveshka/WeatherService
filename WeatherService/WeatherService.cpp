ee3f1d14d1f3cfa992d54a86b6961481f6115dbd
”Anna <abarkova123@mail.ru>
2025-04-23 00:36:32 +0300

    исправлен файл WeatherService.vcxproj - там слияние стрелок надобавляло добавлен файл pugiconfig который сбежал почему - то исправлен main() - убраны дубликаты

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,27 +1,21 @@
-
-п»ї#define PUGIXML_HEADER_ONLY
+#define PUGIXML_HEADER_ONLY
 #include <iostream>
+#include "JsonService.h"
 #include "Weather.h"
 #include "XmlService.h"
 
-
-#include "JsonService.h"
-
 int main()
 {
-    XmlService xs;
-    Weather w = xs.getWeather("weatherXML.txt");
-    w.print();
     setlocale(LC_ALL, "rus");
-    JsonService js;
-    Weather w = js.getWeather("weather.txt");
-    w.print();
 
     JsonService js;
     Weather w_json = js.getWeather("weather.txt");
+    cout << "вывод json\n";
     w_json.print();
 
     XmlService xs;
     Weather w_xml = xs.getWeather("weatherXML.txt");
+    cout << "вывод xml\n";
     w_xml.print();
+
 }
