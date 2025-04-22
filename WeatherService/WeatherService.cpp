e611688b6e2ae961174a172c9649e67355f5d227
”Anna <abarkova123@mail.ru>
2025-04-22 17:44:31 +0300

    Обновление WeatherService.cpp

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,6 +1,19 @@
-﻿#include <iostream>
+﻿#define PUGIXML_HEADER_ONLY
+#include <iostream>
+#include "Weather.h"
+#include "XmlService.h"
+
+
+#include "JsonService.h"
 
 int main()
 {
-    
+    XmlService xs;
+    Weather w = xs.getWeather("weatherXML.txt");
+    w.print();
+    setlocale(LC_ALL, "rus");
+    JsonService js;
+    Weather w = js.getWeather("weather.txt");
+    w.print();
+
 }
