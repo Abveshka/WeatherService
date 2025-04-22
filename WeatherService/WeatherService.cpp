8822a14cadf2e99ae8ce64147bd373f1734e5605
”Anna <abarkova123@mail.ru>
2025-04-22 17:11:29 +0300

    фиксим конфликт

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,26 +1,17 @@
-<<<<<<< HEAD
 ﻿#include <iostream>
 #include "JsonService.h"
+#define PUGIXML_HEADER_ONLY
+#include <iostream>
+#include "Weather.h"
+#include "XmlService.h"
 
 int main()
 {
     setlocale(LC_ALL, "rus");
     JsonService js;
     Weather w = js.getWeather("weather.txt");
-    w.print();
-
-=======
-﻿#define PUGIXML_HEADER_ONLY
-#include <iostream>
-#include "Weather.h"
-#include "XmlService.h"
-
-
-
-int main()
-{
+    w.print();﻿
     XmlService xs;
     Weather w = xs.getWeather("weatherXML.txt");
     w.print();
->>>>>>> feature-xml
 }
