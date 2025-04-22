2d7b73478912f73750a4b313892925bb1a83d365
”Anna <abarkova123@mail.ru>
2025-04-22 16:39:45 +0300

    Added branch feature-xml and class XmlService

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,6 +1,13 @@
-﻿#include <iostream>
+﻿#define PUGIXML_HEADER_ONLY
+#include <iostream>
+#include "Weather.h"
+#include "XmlService.h"
+
+
 
 int main()
 {
-    
+    XmlService xs;
+    Weather w = xs.getWeather("weatherXML.txt");
+    w.print();
 }
